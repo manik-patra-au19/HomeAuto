aaa39a726bf0dd4f9970fa0acd899e505621e5b9
manik-patra-au19 <manikpatra006@gmail.com>
2022-03-30 09:38:36 +0530

    second commit

--- api/models.py
+++ api/models.py
@@ -9,15 +9,15 @@ from django.contrib.auth.models import User
 class Device(models.Model):
     deviceID    = models.CharField(max_length=10,primary_key=True)
     devicetype  = models.CharField(max_length=20)
-    lightStatus = models.ImageField(choices=((0,0),(1,1)),default=0)
-    fanstatus   = models.ImageField(choices=((0,0),(1,1)),default=0)
+    lightStatus = models.IntegerField(choices=((0,0),(1,1)),default=0)
+    fanstatus   = models.IntegerField(choices=((0,0),(1,1)),default=0)
 
     def __str__(self):
         return self.deviceID
 
 class UserDevice(models.Model):
-    User   = models.ForeignKey(User,on_delete=models.CASCADE)
-    Device = models.ForeignKey(Device,on_delete=models.CASCADE)
+    user   = models.ForeignKey(User,on_delete=models.CASCADE)
+    device = models.ForeignKey(Device,on_delete=models.CASCADE)
 
     def __str__(self):
-       return self.user.usename + ''+self.device.deviceId
+       return self.user.username + ' '+self.device.deviceID
--- api/views.py
+++ api/views.py
@@ -10,25 +10,15 @@ def index(request):
     return HttpResponse("this is home")
 
 
-def getdevicestatus(request,pk):
-    
+def getdevicestatus(request,pk):   
     try:
-
         device=Device.objects.get(deviceID=pk)
-        
-        if device.lightStatus==0:
-            device.lightStatus = 1
-
-
 
         dict_ ={"deviceId":device.deviceID,
-                "light":device.lightstatus,
+                "light":device.lightStatus,
                 "fan":device.fanstatus}
-
-
-        return JsonResponse(device.values()[0])
+        return JsonResponse(dict_)
     except Device.DoesNotExist:
-
         return JsonResponse({"message":"no device found"})
 
 def togglelight(request,pk):
@@ -38,15 +28,16 @@ def togglelight(request,pk):
         
         if device.lightStatus==0:
             device.lightStatus = 1
-
-
+        else:
+            device.lightStatus = 0
+        device.save()
 
         dict_ ={"deviceId":device.deviceID,
-                "light":device.lightstatus,
+                "light":device.lightStatus,
                 "fan":device.fanstatus}
 
         
-        return JsonResponse(device.values()[0])
+        return JsonResponse(dict_)
     except Device.DoesNotExist:
 
         return JsonResponse({"message":"no device found"})
@@ -56,17 +47,15 @@ def togglefan(request,pk):
 
         device=Device.objects.get(deviceID=pk)
         
-        if device.fanStatus==0:
-            device.fanStatus = 1
-
-
-
+        if device.fanstatus==0:
+            device.fanstatus = 1
+        else:
+            device.fanstatus = 0
+        device.save()
         dict_ ={"deviceId":device.deviceID,
-                "light":device.lightstatus,
-                "fan":device.fanstatus}
-
-        
-        return JsonResponse(device.values()[0])
+                "light":device.lightStatus,
+                "fan":device.fanstatus}       
+        return JsonResponse(dict_)
 
     except Device.DoesNotExist:
 
